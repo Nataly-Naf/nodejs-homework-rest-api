9a3ef031e979c122605e3f11b348bd99d622a16a
Nataly-Naf <nat.spivak84@gmail.com>
2023-12-22 13:07:50 +0000

    mongoose

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import app from './app.js'
-// mongoDB DGLx2mWYuX8UuNdj
+// mongoDB lGNw81p609yhHuOo
 // natspivak84
-app.listen(3000, () => {
+import mongoose from 'mongoose'
+
+const DB_HOST = "mongodb+srv://Nataly:lGNw81p609yhHuOo@cluster0.afqrzic.mongodb.net/my-contacts?retryWrites=true&w=majority"
+mongoose.connect()
+  .then(() => { app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
-})
+})})
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1)
+  })
+
